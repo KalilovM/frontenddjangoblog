d5c18aa5994e3ccbedd5ef3ddaffa1dd299f4541
KalilovM <57083458+KalilovM@users.noreply.github.com>
2022-11-23 21:52:52 +0600

    Prettified editor, navbar and single post
    Added navbar dropdown menu

--- src/componenets/navbar/navbar.jsx
+++ src/componenets/navbar/navbar.jsx
@@ -2,78 +2,83 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./navbar.module.scss";
 import profile from "../../static/img/user-logo.svg";
+import { observer } from "mobx-react-lite";
+import auth from "../../store/auth";
 
-function Navbar() {
-    const [isAuth, setIsAuth] = React.useState(false);
-    const [clicked, setClicked] = React.useState(false);
-    React.useEffect(() => {
-        const access = localStorage.getItem("access");
-        const refresh = localStorage.getItem("refresh");
+const Navbar = observer(() => {
+  const [clicked, setClicked] = React.useState(false);
+  React.useEffect(() => {
+    const access = localStorage.getItem("access");
+    const refresh = localStorage.getItem("refresh");
 
-        if (!access && !refresh) {
-            setIsAuth(false);
-        }
-        setIsAuth(true);
-    }, []);
+    auth.loggedIn();
+    if (!access && !refresh) {
+      auth.logout();
+    }
+  }, []);
 
-    const Logout = () => {
-        localStorage.removeItem("access");
-        localStorage.removeItem("refresh");
-        navigate("/auth");
-    };
-    const ClickedDrop = () => {
-        setClicked((prev) => !prev);
-    };
-    const navigate = useNavigate();
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.container}>
-                <ul className={styles.navbar_items}>
-                    <li>
-                        <Link to="#">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="#">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="#">Home</Link>
-                    </li>
-                </ul>
-                {!isAuth ? (
-                    <ul className={styles.active_buttons}>
-                        <Link to="/auth" className={styles.auth}>
-                            <li>Login</li>
-                        </Link>
-                        <Link to="/signup" className={styles.signup}>
-                            <li>Signup</li>
-                        </Link>
-                    </ul>
-                ) : (
-                    <div
-                        onClick={ClickedDrop}
-                        className={styles.dropdown_menu}
-                        style={{
-                            height: 30,
-                            width: 30,
-                            background: `url(${profile})`,
-                            backgroundPosition: "center",
-                            backgroundSize: "cover",
-                            backgroundRepeat: "no-repeat",
-                        }}
-                    >
-                        <div
-                            className={styles.dropdown_content}
-                            style={{ opacity: clicked ? 1 : 0 }}
-                        >
-                            <Link to="/profile">Profile</Link>
-                            <Link to="/create">Create post</Link>
-                            <button onClick={Logout}>Logout</button>
-                        </div>
-                    </div>
-                )}
+  const Logout = () => {
+    localStorage.removeItem("access");
+    localStorage.removeItem("refresh");
+    auth.logout();
+    navigate("/auth");
+  };
+  const ClickedDrop = () => {
+    setClicked((prev) => !prev);
+  };
+  const navigate = useNavigate();
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <ul className={styles.navbar_items}>
+          <li>
+            <Link to="#">Home</Link>
+          </li>
+          <li>
+            <Link to="#">Home</Link>
+          </li>
+          <li>
+            <Link to="#">Home</Link>
+          </li>
+        </ul>
+        {!auth.isAuth ? (
+          <ul className={styles.active_buttons}>
+            <Link to="/auth" className={styles.auth}>
+              <li>Login</li>
+            </Link>
+            <Link to="/signup" className={styles.signup}>
+              <li>Signup</li>
+            </Link>
+          </ul>
+        ) : (
+          <div
+            onClick={ClickedDrop}
+            className={styles.dropdown_menu}
+            style={{
+              height: 30,
+              width: 30,
+              background: `url(${profile})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            <div
+              className={styles.dropdown_content}
+              style={{
+                maxHeight: clicked ? 200 : 0,
+                borderWidth: clicked ? 2 : 0,
+              }}
+            >
+              <Link to="/profile">Profile</Link>
+              <Link to="/create">Create post</Link>
+              <button onClick={Logout}>Logout</button>
             </div>
-        </div>
-    );
-}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+});
 
 export default Navbar;
--- src/componenets/post/single/single.jsx
+++ src/componenets/post/single/single.jsx
@@ -1,37 +1,33 @@
-import React from 'react';
-import styles from './single.module.scss'
-import {InstanceApi} from "../../../sevices/axios/axios";
-import {useParams} from "react-router-dom";
+import React from "react";
+import styles from "./single.module.scss";
+import { InstanceApi } from "../../../services/axios/axios";
+import { useParams } from "react-router-dom";
 import Avatar from "../avatar/avatar";
-import Output from 'editorjs-react-renderer'
+import Output from "editorjs-react-renderer";
 
 function SinglePost(props) {
-    const {id} = useParams()
-    console.log(id)
-    const [postData, setPostData] = React.useState(null)
+    const { id } = useParams();
+    console.log(id);
+    const [postData, setPostData] = React.useState(null);
 
     React.useEffect(() => {
         InstanceApi.get(`/${id}/`).then((res) => {
-            console.log(res.data)
-            setPostData(res.data)
-        })
-    }, [])
+            console.log(res.data);
+            setPostData(res.data);
+        });
+    }, []);
 
     return (
-        !!postData &&
-        <div className={styles.wrapper}>
-            <Avatar {...postData.author} date={postData.created_at}/>
-            <h1 className={styles.heading}>{postData.title}</h1>
-            <div className={styles.image_container}>
-                <img
-                    src={postData.cover}
-                    alt="cover"
-                />
+        !!postData && (
+            <div className={styles.wrapper}>
+                <Avatar {...postData.author} date={postData.created_at} />
+                <h1 className={styles.heading}>{postData.title}</h1>
+                <div className={styles.image_container}>
+                    <img src={postData.cover} alt="cover" />
+                </div>
+                <Output data={postData.content} />
             </div>
-            <Output data={postData.content}/>
-
-
-        </div>
+        )
     );
 }
 
