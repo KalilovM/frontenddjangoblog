a093bb1556caaaaf21921570748b133f6a291bd5
KalilovM <57083458+KalilovM@users.noreply.github.com>
2022-11-25 16:56:26 +0600

    Made statuses of creating post more pretty
    Created function to better formatting date time type
    
    Reformatted avatar.jsx

--- src/componenets/post/avatar/avatar.jsx
+++ src/componenets/post/avatar/avatar.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import styles from "./avatar.module.scss"
+import React from "react";
+import styles from "./avatar.module.scss";
+import { DateTimeFormatter } from "../../../helpers/DateTimeFormatter";
 
 function Avatar(props) {
-    console.log(props)
     return (
         <div className={styles.container}>
-            <img
-                src={props.avatar}
-                alt="avatar"
-            />
-            <p><b>{props.username}</b></p>
-            <p className={styles.date_time}>{Date(props.date)}</p>
+            <img src={props.avatar} alt="avatar" />
+            <p>
+                <b>{props.username}</b>
+            </p>
+            <p className={styles.date_time}>{DateTimeFormatter(props.date)}</p>
         </div>
     );
 }
